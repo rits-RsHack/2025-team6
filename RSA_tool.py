ead2583a13a8e0b585c3e49c6b128417f07efb16
Tubo <yuki_tubotani@outlook.jp>
2025-09-08 13:30:46 +0900

    factorDBで解けない場合の関数実装

--- RSA_tool.py
+++ RSA_tool.py
@@ -1,5 +1,9 @@
 from Crypto.Util.number import long_to_bytes
 import requests
+from gmpy2 import iroot
+import math
+from math import gcd
+import owiener
 
 
 #factordbとのAPI処理
@@ -37,14 +41,83 @@ def factordb(number):
 
 
 
-def solve(p, q, e, ct):    #解読プログラム
+def solve(p, q, e, ct):    #通常攻撃プログラム
+
     try:
         phi = (p-1) * (q-1)
         d = pow(e, -1, phi)
         plain_hex = pow(ct, d, p*q)
         plain_text = long_to_bytes(plain_hex)
-        return plain_text.decode(errors = "ignore")
+        return plain_text.decode()
     except Exception as e:
         print(f"Error has occurred: {e}")
         return "Decryption failed"
         #復号失敗
+
+
+
+def low_index(e, ct): #低指数攻撃プログラム
+
+    try:    
+        plain_hex,exact = iroot(e, ct)
+
+        if exact:
+            plain_text = long_to_bytes(plain_hex)
+            return plain_text.decode()
+    except Exception as e:
+        print(f"Error has occurred: {e}")
+        return "Decryption failed" #復号失敗
+
+
+
+def super_low_index(N, e, ct):
+
+    if e == 1:
+        plain_hex = pow(ct, e, N)
+        plain_text = long_to_bytes(plain_hex)
+        return plain_text.decode()
+    else:
+        return "Decryption failed" #復号失敗
+
+
+def squared_index(N, e, ct):
+
+    try:    
+        f = math.isqrt(n)
+        phi = (f-1)*f
+        d = pow(e, -1, phi)
+        plain_hex = pow(ct, d, N)
+        plain_text = long_to_bytes(plain_hex)
+        return plain_text.decode()
+    except Exception as e:
+        print(f"Error has occurred: {e}")
+        return "Decryption failed" #復号失敗
+
+
+def N_prime(N, e, ct):
+
+    try:    
+        f = math.isqrt(n)
+        phi = N-1
+        d = pow(e, -1, phi)
+        plain_hex = pow(ct, d, N)
+        plain_text = long_to_bytes(plain_hex)
+        return plain_text.decode()
+    except Exception as e:
+        print(f"Error has occurred: {e}")
+        return "Decryption failed" #復号失敗
+
+
+def Wiener(N, e, ct):
+
+    d = owiener.attack(e, N)
+    if d is None:
+        return "Decryption failed" #復号失敗
+    else:
+        try:
+            plain_hex = pow(ct, d, N)
+            plain_text = long_to_bytes(plain_hex)
+            return plain_text.decode()
+        except Exception as e:
+            print(f"Error has occurred: {e}")
+            return "Decryption failed" #復号失敗
