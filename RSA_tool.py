414492044831a3d656a4ff7e33850717025abf39
Tubo <yuki_tubotani@outlook.jp>
2025-09-09 11:37:30 +0900

    Fermat法の関数の追加

--- RSA_tool.py
+++ RSA_tool.py
@@ -3,6 +3,7 @@ import requests
 from gmpy2 import iroot
 import math
 from math import gcd
+from math import isqrt, ceil
 import owiener
 import subprocess
 import os
@@ -108,7 +109,7 @@ def solve(p, q, e, ct):    #通常攻撃プログラム
 
 
 
-def low_index(e, ct): #低指数攻撃プログラム
+def low_index(N, e, ct): #低指数攻撃プログラム
 
     try:    
         plain_hex,exact = iroot(e, ct)
@@ -120,15 +121,6 @@ def low_index(e, ct): #低指数攻撃プログラム
         print(f"Error has occurred: {e}")
         return "Decryption failed" #復号失敗
 
-
-
-def super_low_index(e, ct):
-
-    if e == 1:
-        plain_text = long_to_bytes(ct)
-        return plain_text.decode()
-    else:
-        return "Decryption failed" #復号失敗
 
 
 def squared_index(N, e, ct):
@@ -173,9 +165,38 @@ def Wiener(N, e, ct):
             print(f"Error has occurred: {e}")
             return "Decryption failed" #復号失敗
 
-ct = 320721490534624434149993723527322977960556510750628354856260732098109692581338409999983376131354918370047625150454728718467998870322344980985635149656977787964380651868131740312053755501594999166365821315043312308622388016666802478485476059625888033017198083472976011719998333985531756978678758897472845358167730221506573817798467100023754709109274265835201757369829744113233607359526441007577850111228850004361838028842815813724076511058179239339760639518034583306154826603816927757236549096339501503316601078891287408682099750164720032975016814187899399273719181407940397071512493967454225665490162619270814464
-N = 580642391898843192929563856870897799650883152718761762932292482252152591279871421569162037190419036435041797739880389529593674485555792234900969402019055601781662044515999210032698275981631376651117318677368742867687180140048715627160641771118040372573575479330830092989800730105573700557717146251860588802509310534792310748898504394966263819959963273509119791037525504422606634640173277598774814099540555569257179715908642917355365791447508751401889724095964924513196281345665480688029639999472649549163147599540142367575413885729653166517595719991872223011969856259344396899748662101941230745601719730556631637
-e = 65537
 
-result = yafu_factor(N, e, ct)
-print(result)
+def is_square(n):
+    s = isqrt(n)
+    return s * s == n
+
+def Fermat_factor(N, e, ct):
+
+    max_diff=10**6
+    a = isqrt(N)
+    if a * a < N:
+        a += 1
+
+    limit = max_diff // 2
+    
+    for _ in range(limit):
+        diff = a*a - N
+        if is_square(diff):  # b^2 なら即 return
+            b = isqrt(diff)
+            p = a + b
+            q = a - b
+        
+            try:
+                phi = (p-1) * (q-1)
+                d = pow(e, -1, phi)
+                plain_hex = pow(ct, d, p*q)
+                plain_text = long_to_bytes(plain_hex)
+                return plain_text.decode()
+            except Exception as e:
+                print(f"Error has occurred: {e}")
+                return "Decryption failed"
+                #復号失敗
+
+        a += 1
+    
+    return "Factors not found within max_diff limit"
